b11eb7f7089474dd576a254acc1977ee5ee5a1fe
Sofiaantenor <90793659+Sofiaantenor@users.noreply.github.com>
2022-06-10 16:08:31 -0300

    Add files via upload

---
+++ AMS_Ex4_22_/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AMS_Ex4_22_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grausCelsius;
+
+            Console.WriteLine("");
+            Console.WriteLine("    ***CONVERSÃO DE GRAUS CELSIUS PARA FAHRENHEIT***      ");
+
+            Console.Write("Digite o valor em graus Celsius que deseja converter para Fahrenheit: ");
+            grausCelsius = Convert.ToDouble(Console.ReadLine());
+
+            double fahrenheit = (9 * grausCelsius + 160) / 5;
+
+            Console.WriteLine($"O valor equivalente a {grausCelsius} graus Celsius em Fahrenheit é {fahrenheit}");
+            Console.WriteLine("");
+        }
+    }
+}
