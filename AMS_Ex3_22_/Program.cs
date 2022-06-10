f04599172dda06c4be636ae2bc27ab3f0d2e9cfe
Sofiaantenor <90793659+Sofiaantenor@users.noreply.github.com>
2022-06-10 16:07:14 -0300

    Add files via upload

---
+++ AMS_Ex3_22_/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AMS_Ex3_22_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorVendido;
+            int porcentagem;
+
+            Console.WriteLine("");
+            Console.WriteLine("          ***CÁLCULO DE COMISSÃO DE VENDAS***      ");
+
+            Console.Write("Digite o valor que foi vendido: ");
+            valorVendido = Convert.ToInt32(Console.ReadLine()); 
+
+            Console.Write("Digite o número da porcentagem da comissão do funcionário: ");
+            porcentagem = Convert.ToInt32(Console.ReadLine()); 
+
+            int resultado = valorVendido * porcentagem /100;
+
+            Console.WriteLine("");
+            Console.WriteLine($"O valor da comissão desse vendedor será de: {resultado}");
+            Console.WriteLine("");
+        }
+    }
+}
