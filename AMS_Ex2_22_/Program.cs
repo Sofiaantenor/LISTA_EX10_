752dc9eccf2f07bb1549f215fcdd509990af28ca
Sofiaantenor <90793659+Sofiaantenor@users.noreply.github.com>
2022-06-10 16:06:12 -0300

    Add files via upload

---
+++ AMS_Ex2_22_/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AMS_Ex2_22_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double euro;
+            double qtdEuro;
+            
+            Console.WriteLine("");
+            Console.WriteLine("     ***CONVERÇÃO DE EUROS PARA O REAL BRASILEIRO***      ");
+
+            Console.Write("Digite o atual valor do euro: ");
+            euro = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor em euro que deseja converter: ");
+            qtdEuro = Convert.ToDouble(Console.ReadLine()); 
+
+            double real = (qtdEuro * euro);
+
+            Console.WriteLine($"O valor dessa quantia em euro convertida para o real é de: {real} reais");
+            Console.WriteLine("");
+        }
+    }
+}
