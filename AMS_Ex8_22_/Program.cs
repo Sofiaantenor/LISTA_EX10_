4a8d20e7ee5739a964151b949bfbfc59d93b0111
Sofiaantenor <90793659+Sofiaantenor@users.noreply.github.com>
2022-06-10 16:17:03 -0300

    Add files via upload

---
+++ AMS_Ex8_22_/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AMS_Ex8_22_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double v1;
+            double v2;
+
+            Console.WriteLine("");
+            Console.WriteLine("         ***CÁLCULO DE SUBTRAÇÃO ENTRE DOIS VALORES***      ");
+
+            Console.Write("Digite o primeiro valor: ");
+            v1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o segundo valor: ");
+            v2 = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = v1 - v2;
+
+            if( resultado < 0)
+            {
+               Console.WriteLine($"O resultado dessa subtração é negativo! O número resultante é: {resultado}");
+            }
+            else if(resultado > 0)
+            {
+               Console.WriteLine($"O resultado dessa subtração é positivo! O número resultante é: {resultado}");
+            }
+            else 
+            {
+               Console.WriteLine("O resultado dessa subtração é zero!");
+            }
+
+            Console.WriteLine("");
+
+        }
+    }
+}
