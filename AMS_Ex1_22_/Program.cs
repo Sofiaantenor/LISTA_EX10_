47e307558d4b2dbdd9929795802dbd649f4b9507
Sofiaantenor <90793659+Sofiaantenor@users.noreply.github.com>
2022-06-10 16:04:44 -0300

    Add files via upload

---
+++ AMS_Ex1_22_/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AMS_Ex1_22_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idadeMinima;
+            int idadeMaxima;
+
+            Console.WriteLine("");
+            Console.WriteLine("          ***CÁLCULO DE MÉDIA ENTRE DUAS IDADES***      ");
+
+            Console.Write("Digite a idade máxima: ");
+            idadeMaxima = Convert.ToInt16(Console.ReadLine()); 
+
+            Console.Write("Digite a idade mínima: ");
+            idadeMinima = Convert.ToInt16(Console.ReadLine()); 
+
+            int mediaIdade = (idadeMinima + idadeMaxima)/2;
+            
+            Console.WriteLine($"A média dessas idades é: {mediaIdade}");
+            Console.WriteLine("");
+        }
+    }
+}
