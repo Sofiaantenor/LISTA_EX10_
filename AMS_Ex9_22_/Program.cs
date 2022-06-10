094664e48a4bcdadc0fce9ce101bbea23b1bfedc
Sofiaantenor <90793659+Sofiaantenor@users.noreply.github.com>
2022-06-10 16:17:57 -0300

    Add files via upload

---
+++ AMS_Ex9_22_/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AMS_Ex9_22_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double v1;
+            double v2;
+            
+            Console.WriteLine("");
+            Console.WriteLine("        ***CÁLCULO DO RESTO DE UMA DIVISÃO ENTRE DOIS VALORES***      ");
+
+            Console.Write("Digite o primeiro valor: ");
+            v1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o segundo valor: ");
+            v2 = Convert.ToDouble(Console.ReadLine());
+            
+            double resto = v1 % v2;
+
+            if (v2 == 0)
+            {
+               Console.WriteLine("Não é possível realizar uma divisão por 0!");
+            }
+            else
+            {
+               Console.WriteLine($"O resto da divisão entre {v1} e {v2} é {resto}");
+            }
+
+            Console.WriteLine("");
+        }
+    }
+}
